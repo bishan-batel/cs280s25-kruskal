492c14fa39ffb72c8750d21769335cf2081d011a
Kishan S Patel <bishan.batel@protonmail.com>
2025-04-14 11:38:28 -0700

    compiles

--- disjoint_sets.cpp
+++ disjoint_sets.cpp
@@ -1,91 +1,81 @@
 #include "disjoint_sets.h"
 
-//class Node implementation
-Node::Node( size_t const& value ) : value(value), next() {}
+// class Node implementation
+Node::Node(size_t value) : next(), value(value) {}
 
-Node* Node::Next() const { return next; }
+Node *Node::get_next() const { return next; }
 
-void  Node::SetNext( Node* new_next ) { next = new_next; }
+void Node::set_next(Node *new_next) { next = new_next; }
 
-size_t Node::Value() const { return value; }
+size_t Node::get_value() const { return value; }
 
-std::ostream& operator<< (std::ostream& os, Node const& node) {
-	os << "(" << node.value << " ) -> ";
-	return os;
+std::ostream &operator<<(std::ostream &os, Node const &node) {
+  os << "(" << node.value << " ) -> ";
+  return os;
 }
 
-//class Head implementation
-Head::Head( ) : counter(), first(), last() {} 
+// class Head implementation
+Head::Head() = default;
 
-Head::~Head() {
-	/* .... */
+Head::~Head() { /* .... */ }
+
+size_t Head::size() const { return counter; }
+
+void Head::reset() {
+  counter = 0;
+  last = nullptr;
+  first = nullptr;
 }
 
-size_t Head::Size() const      { return counter; }
+Node *Head::get_first() const { return first; }
 
-void   Head::Reset()           { counter = 0; last = first = NULL; }
+Node *Head::get_last() const { return last; }
 
-Node*  Head::GetFirst() const  { return first; }
-
-Node*  Head::GetLast()  const  { return last; }
-
-void   Head::Init( size_t value ) {
-	first = last = new Node( value );
-	counter = 1;
+void Head::init(size_t value) {
+  first = last = new Node{value};
+  counter = 1;
 }
 
-void   Head::Join( Head* pHead2 ) {
-	/* .... */
+void Head::join(Head *head2) {}
+
+std::ostream &operator<<(std::ostream &os, Head const &head) {
+  os << "[" << head.counter << " ] -> ";
+  return os;
 }
 
-std::ostream& operator<< (std::ostream& os, Head const& head) {
-	os << "[" << head.counter << " ] -> ";
-	return os;
-}
-
-//class DisjointSets implementation
-DisjointSets::DisjointSets( size_t const& capacity ) : 
-	size(0),
-	capacity(capacity),
-	representatives(new size_t[capacity]),
-	heads          (new Head[capacity])
-{ }
+// class DisjointSets implementation
+DisjointSets::DisjointSets(size_t const &capacity) :
+    size(0), capacity(capacity), representatives(new size_t[capacity]), heads(new Head[capacity]) {}
 
 DisjointSets::~DisjointSets() {
-	delete [] heads;
-	delete [] representatives;
+  delete[] heads;
+  delete[] representatives;
 }
 
-void DisjointSets::Make( ) {
-	if ( size == capacity ) throw "DisjointSets::Make(...) out of space";
-	heads[size].Init( size );
-	representatives[size] = size;
-	++size;
+void DisjointSets::Make() {
+  if (size == capacity) throw "DisjointSets::Make(...) out of space";
+  heads[size].init(size);
+  representatives[size] = size;
+  ++size;
 }
 
-void DisjointSets::Join( size_t const& id1, size_t const& id2 ) {
-	/* .... */
-}
+void DisjointSets::Join(size_t const &id1, size_t const &id2) {}
 
-size_t DisjointSets::GetRepresentative( size_t const& id ) const {
-	return representatives[id];
-}
+size_t DisjointSets::GetRepresentative(size_t const &id) const { return representatives[id]; }
 
-size_t DisjointSets::operator[]( size_t const& id ) const {
-	return representatives[id];
-}
+size_t DisjointSets::operator[](size_t const &id) const { return representatives[id]; }
 
-std::ostream& operator<< (std::ostream& os, DisjointSets const& ds) {
-	for (size_t i=0; i<ds.size; ++i ) {
-		os << i << ":  ";
-		Head *p_head = &ds.heads[i];
-		os << *p_head;
-		Node* p_node = p_head->GetFirst();
-		while ( p_node ) {
-			os << *p_node;
-			p_node = p_node->Next();
-		}
-		os << "NULL (representative " << ds.representatives[i] << ")\n";
-	}
-	return os;
+std::ostream &operator<<(std::ostream &os, DisjointSets const &ds) {
+  for (size_t i = 0; i < ds.size; ++i) {
+    os << i << ":  ";
+    Head *p_head = &ds.heads[i];
+    os << *p_head;
+    Node *p_node = p_head->get_first();
+    while (p_node) {
+      os << *p_node;
+      p_node = p_node->get_next();
+    }
+    os << "NULL (representative " << ds.representatives[i] << ")\n";
+  }
+  return os;
 }
--- disjoint_sets.h
+++ disjoint_sets.h
@@ -1,15 +1,15 @@
 /*!
-  \brief  Disoint sets using quick find implementation (linked lists and 
+  \brief  Disoint sets using quick find implementation (linked lists and
   a look-up table)
 
 Implements:
-	ctor (size)
-	Make( id )      initialize
-	Join( id,id )   join 2 sets
-	GetRepresentative( id )
+  ctor (size)
+  Make( id )      initialize
+  Join( id,id )   join 2 sets
+  GetRepresentative( id )
 
 Rationale:
-	elements of the set are assumed to be contiguous 0,1,2,3,....
+  elements of the set are assumed to be contiguous 0,1,2,3,....
 */
 
 #ifndef DISJOINT_SETS_H
@@ -17,30 +17,81 @@ Rationale:
 #include <cstdlib>
 #include <fstream> //ostream, istream
 
-// add Head and Node 
+// add Head and Node
 
+class Node final {
+public:
+  Node(size_t value);
 
+  Node *get_next() const;
+
+  void set_next(Node *new_next);
+
+  size_t get_value() const;
+
+  friend std::ostream &operator<<(std::ostream &os, Node const &node);
+
+private:
+  Node *next;
+  size_t value;
+};
+
+class Head final {
+public:
+  Head();
+
+  ~Head();
+
+  size_t size() const;
+
+  void reset();
+
+  Node *get_first() const;
+
+  Node *get_last() const;
+
+  void init(size_t value);
+
+  void join(Head *pHead2);
+
+  friend std::ostream &operator<<(std::ostream &os, Head const &head);
+
+private:
+  size_t counter{0};
+  Node *first{nullptr};
+  Node *last{nullptr};
+};
 
 ////////////////////////////////////////////////////////////
 class DisjointSets { // you MAY modify this
-	public:
-		DisjointSets( size_t const& capacity );
-		DisjointSets( DisjointSets const& ) = delete;
-		DisjointSets& operator=( DisjointSets const& ) = delete;
-		DisjointSets( DisjointSets && ) = delete;
-		DisjointSets& operator=( DisjointSets && ) = delete;
-		~DisjointSets();
-		void   Make( );
-		void   Join( size_t const& id1, size_t const& id2 );
-		size_t GetRepresentative( size_t const& id ) const;
-		size_t operator[]( size_t const& id ) const;
-		friend std::ostream& operator<< (std::ostream& os, DisjointSets const& ds);
-	private:
-		size_t  size;            // current size
-		size_t  capacity;        // capacity - NOT growing, provided as ctor arg
-		size_t *representatives; // look-up table ID -> representative's ID
-		Head   *heads;           // lists' heads
-};
+public:
+  DisjointSets(size_t const &capacity);
 
+  DisjointSets(DisjointSets const &) = delete;
+
+  DisjointSets &operator=(DisjointSets const &) = delete;
+
+  DisjointSets(DisjointSets &&) = delete;
+
+  DisjointSets &operator=(DisjointSets &&) = delete;
+
+  ~DisjointSets();
+
+  void Make();
+
+  void Join(size_t const &id1, size_t const &id2);
+
+  size_t GetRepresentative(size_t const &id) const;
+
+  size_t operator[](size_t const &id) const;
+
+  friend std::ostream &operator<<(std::ostream &os, DisjointSets const &ds);
+
+private:
+  size_t size{0}; // current size
+  size_t capacity{0}; // capacity - NOT growing, provided as ctor arg
+  size_t *representatives{nullptr}; // look-up table ID -> representative's ID
+  Head *heads{nullptr}; // lists' heads
+};
 
 #endif
